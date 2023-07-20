8ff6ac00d99dbf622978dee65b801a899db53d94
Yanni Kouloumbis <107221725+YanniKouloumbis@users.noreply.github.com>
2023-07-20 03:29:14 +0300

    change high

--- app/page.tsx
+++ app/page.tsx
@@ -1,87 +1,86 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
-import CanvasComponent from "@/components/canvas";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Loader } from "lucide-react";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+"use client"
+
+import { useEffect, useRef, useState } from "react"
+import { useRouter, useSearchParams } from "next/navigation"
+import { Loader } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import { useToast } from "@/components/ui/use-toast";
-import { useRouter } from 'next/navigation'
-import { useSearchParams } from 'next/navigation'
+} from "@/components/ui/select"
+import { useToast } from "@/components/ui/use-toast"
+import CanvasComponent from "@/components/canvas"
 
 export default function Home() {
   const router = useRouter()
   const searchParams = useSearchParams()
-  const { toast } = useToast();
+  const { toast } = useToast()
   const [objectLink, setObjectLink] = useState<string>(
     "A chair shaped like an avocado.ply"
-  );
-  const [inputText, setInputText] = useState("");
-  const [generating, setGenerating] = useState<boolean>(false);
-  const [numInferenceSteps, setNumInferenceSteps] = useState<number>(32);
-  const [openrouterApiKey, setOpenrouterApiKey] = useState<string>("");
-  const ai = useRef<any>(null);
+  )
+  const [inputText, setInputText] = useState("")
+  const [generating, setGenerating] = useState<boolean>(false)
+  const [numInferenceSteps, setNumInferenceSteps] = useState<number>(32)
+  const [openrouterApiKey, setOpenrouterApiKey] = useState<string>("")
+  const ai = useRef<any>(null)
 
   useEffect(() => {
-    if(localStorage.getItem('openrouterApiKey')){
-      setOpenrouterApiKey(localStorage.getItem('openrouterApiKey') as string)
-    }
-    else{
-      const code = searchParams.get('code')
-      if(code){
+    const openrouterApiKeyFromStorage = localStorage.getItem('openrouterApiKey');
+    if (openrouterApiKeyFromStorage) {
+      setOpenrouterApiKey(openrouterApiKeyFromStorage);
+    } else {
+      const codeFromSearchParams = searchParams.get('code');
+      if (codeFromSearchParams) {
         fetch("https://openrouter.ai/api/v1/auth/keys", {
           method: 'POST',
-          body: JSON.stringify({
-            code: code,
-          })
-        }).then((res) => {
-          return res.json()
-        }
-        ).then((res) => {
-          if(res.key){
+          body: JSON.stringify({ code: codeFromSearchParams })
+        })
+        .then(res => res.json())
+        .then(res => {
+          if (res.key) {
             localStorage.setItem('openrouterApiKey', res.key);
           }
-        }
-        ).catch((err) => {
-          console.error(err)
         })
+        .catch(err => console.error(err));
+      }
     }
-    }
-}, [])
-
-  // Generate 3D object with openrouter. Supply a prompt and number of inference steps and receive a data URI.
-  const generate3DObject = async () => {
+  }, []);
+  
+  // Sends POST request to generate a 3D object based on prompt and number of inference steps provided as input.
+  const generate3DObject = async (): Promise<string> => {
     const output = await fetch("https://openrouter.ai/api/v1/media/generations", {
       method: 'POST',
       body: JSON.stringify({
         prompt: inputText,
         numInferenceSteps: numInferenceSteps
-        }),
+      }),
       headers: {
         "HTTP-Referer": "https://test.com",
         "X-Title": "test",
         "Authorization": `Bearer ${openrouterApiKey}`
       }
-    })
-    const generations = await output.json()
+    });
+    const generations = await output.json();
     return generations.data[0].uri;
   };
-
-  // Handle generation.
+  
+  /** 
+   * Handle the generation of a 3D object and sets the URI to the 'objectLink' state.
+   * @async
+   */
   const handleGenerate = async () => {
     try {
       setGenerating(true);
-      if(!openrouterApiKey){
+      if (!openrouterApiKey) {
         toast({ title: "Please login with Openrouter." });
-        return
+        return;
       }
       const dataUri = await generate3DObject();
       setObjectLink(dataUri);
@@ -92,10 +91,13 @@ export default function Home() {
     }
   };
   
+  /** 
+   * Handle download of the 3D object by creating a temporary link on DOM and initiating a click on it.
+   */
   const handleDownload = () => {
     const link = document.createElement("a");
     link.href = objectLink as string;
-    link.download = inputText + ".ply";
+    link.download = `${inputText}.ply`;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -114,7 +116,9 @@ export default function Home() {
             <Label htmlFor="numInferenceSteps">Quality</Label>
             <div className="mb-5">
               <Select
-                onValueChange={(value: string) => setNumInferenceSteps(parseInt(value))}
+                onValueChange={(value: string) =>
+                  setNumInferenceSteps(parseInt(value))
+                }
                 defaultValue="32"
               >
                 <SelectTrigger className="w-[180px]">
@@ -123,7 +127,7 @@ export default function Home() {
                 <SelectContent>
                   <SelectItem value="16">Low</SelectItem>
                   <SelectItem value="32">Medium</SelectItem>
-                  <SelectItem value="64">High</SelectItem>
+                  <SelectItem value="54">High</SelectItem>
                 </SelectContent>
               </Select>
             </div>
@@ -135,16 +139,25 @@ export default function Home() {
                 Download Model
               </Button>
             </div>
-            <Button className="mt-2" onClick={()=> {
-              if(openrouterApiKey){
-              localStorage.removeItem('openrouterApiKey')
-              setOpenrouterApiKey('')
-              }
-              else{
-                router.push(`https://openrouter.ai/auth?callback_url=${window.location.origin + window.location.pathname}`)}
-              }}>
-                {openrouterApiKey ? "Clear Openrouter API Key" : "Login With Openrouter"}
-              </Button>
+            <Button
+              className="mt-2"
+              onClick={() => {
+                if (openrouterApiKey) {
+                  localStorage.removeItem("openrouterApiKey")
+                  setOpenrouterApiKey("")
+                } else {
+                  router.push(
+                    `https://openrouter.ai/auth?callback_url=${
+                      window.location.origin + window.location.pathname
+                    }`
+                  )
+                }
+              }}
+            >
+              {openrouterApiKey
+                ? "Clear Openrouter API Key"
+                : "Login With Openrouter"}
+            </Button>
           </div>
           <div className="w-full md:w-1/2 h-full overflow-auto p-1">
             {objectLink && <CanvasComponent objectLink={objectLink} />}
@@ -152,5 +165,5 @@ export default function Home() {
         </CardContent>
       </Card>
     </div>
-  );
+  )
 }
